b5b93a021f8b8a1d8c5838e82f09165f1326064e
SimoneIOLST <282324@studenti.unimore.it>
2023-08-23 10:57:47 +0200

    Fine pagina fittizia

--- tecweb/views.py
+++ tecweb/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 def home_page(request):
     lista=range(1,39)
-    ctx = {"lista": lista}
+    num_acc=range(1,39)
+    ctx = {"lista": lista, "num_acc": num_acc}
     return render(request, template_name="base.html", context=ctx)
+
