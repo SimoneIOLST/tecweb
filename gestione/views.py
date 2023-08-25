e17d19a07f1997aa6f782aacec578482dea2ab09
SimoneIOLST <282324@studenti.unimore.it>
2023-08-25 19:15:22 +0200

    Automatic car_id

--- gestione/views.py
+++ gestione/views.py
@@ -54,19 +54,14 @@ class CreateImmaginiMacchinaView(CreateView):
     template_name = 'gestione/creaImmMacc.html'  # Replace with your template name
     
     def get_context_data(self, **kwargs):
-            print("trig")
-            context = super().get_context_data(**kwargs)
-            context['car_id'] = self.kwargs['car_id']
-            return context
+        context = super().get_context_data(**kwargs)
+        context['car_id'] = self.kwargs['car_id']
+        return context
 
 
     def form_valid(self, form):
-            print("Fronte File:", self.request.FILES.get('fronte'))
-            print("Interni File:", self.request.FILES.get('interni'))
-            print("Retro File:", self.request.FILES.get('retro'))
-            
-            form.instance.car_id_id = self.kwargs['car_id']
-            return super().form_valid(form)
+        form.instance.car_id_id = self.kwargs['car_id']
+        return super().form_valid(form)
         
     def get_success_url(self):
         return reverse('gestione:mezzo-list')  # Redirect back to MezzoCreateView
